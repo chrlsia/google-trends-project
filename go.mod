624e77d87e77bb5039bf3b8d64e0a9803a1f7773
Chris Siannas <xrsia@otenet.gr>
2022-04-20 13:45:26 +0300

    Start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chrlsia/google-trends-project
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Item struct {
+	Title     string
+	Link      string
+	Traffic   string
+	NewsItems []News
+}
+
+type News struct {
+	Headline     string
+	HeadlineLink string
+}
+
+func main() {
+	readGoogleTrends
+}
+
+func readGoogleTrends() {
+	getGoogleTrends
+}
+
+func getGoogleTrends() {
+
+}
